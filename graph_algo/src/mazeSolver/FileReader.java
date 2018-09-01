d7016fb9da2147824d154f73871d1b498528896f
bond007 <36125286+sharmavivek223@users.noreply.github.com>
2018-09-01 15:10:32 +0530

    test

--- graph_algo/src/mazeSolver/FileReader.java
+++ graph_algo/src/mazeSolver/FileReader.java
@@ -1,5 +1,9 @@
 package mazeSolver;
 
+import java.io.File;
+import java.util.Scanner;
+
+
 public class FileReader {
 
 	private int[][] map;
@@ -10,21 +14,22 @@ public class FileReader {
 	private int startCol;
 	
 	
-}
-public FileReader(String fileName,int noOfRows,int noOfCols) {
-	this.fileName=fileName;
-	this.noOfCols=noOfCols;
-	this.noOfRows=noOfRows;
-	this.map=new int[noOfRows][noOfCols];
-}
-public void parseFile() {
-	try {
-		Scanner scanner=new Scanner(new File(this.fileName));
-	for(int i=0;i<this.noOfRows;i++) {
-		for(int j=0;j<this.noOfCols;j++) {
-			map[i][j]=scanner.
+	public FileReader(String fileName,int noOfRows,int noOfCols) {
+		this.fileName=fileName;
+		this.noOfCols=noOfCols;
+		this.noOfRows=noOfRows;
+		this.map=new int[noOfRows][noOfCols];
+	}
+	public void parseFile() {
+		try {
+			Scanner scanner=new Scanner(new File(this.fileName));
+		for(int i=0;i<this.noOfRows;i++) {
+			for(int j=0;j<this.noOfCols;j++) {
+				map[i][j]=scanner.nextInt();
+			}
+			
 		}
-		
+		}
 	}
-	}
+	
 }
