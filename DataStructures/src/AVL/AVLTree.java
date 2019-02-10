33ebaccd4dd2b43a98e8e4911b3960a56b8f00a2
Vivek Sharma <Vivek Sharma@DESKTOP-VIVEK>
2019-02-10 12:12:47 +0530

    -traversal,balance implemented

--- DataStructures/src/AVL/AVLTree.java
+++ DataStructures/src/AVL/AVLTree.java
@@ -1,17 +1,43 @@
 package AVL;
 
 public class AVLTree implements Tree{
-
+	private Node root;
 	@Override
 	public void insert(int data) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void traverse() {
-		// TODO Auto-generated method stub
+		if(root==null) {
+			return;
+		}
+		inOrderTraversal(root);
 		
+	}
+	private void inOrderTraversal(Node root) {
+		//Left-Root-Right
+		if(root.getLeftNode()!=null) {
+			inOrderTraversal(root.getLeftNode());
+		}
+		System.out.println(root);
+		if(root.getRightNode()!=null) {
+			inOrderTraversal(root.getRightNode());
+		}
+		
+	}
+	public int height(Node node) {
+		if(node==null) {
+			return -1;
+		}
+		return node.getHeight();
+	}
+	public int getBalance(Node node) {
+		if(node==null) {
+			return 0;
+		}
+		return height(node.getLeftNode())-height(node.getRightNode());
 	}
 
 }
