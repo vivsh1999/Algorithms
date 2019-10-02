fa6d8f4fed387070ed2b341eb60ec5ad57ec8aec
SakshiGupta2501 <sakshigupta199938@gmail.com>
2019-10-02 11:12:06 +0000

    little improvements

--- DataStructures/src/AVL/Tree.java
+++ DataStructures/src/AVL/Tree.java
@@ -1,5 +1,6 @@
 package AVL;
 
+//an interface for tree
 public interface Tree {
 	public void insert(int data);
 	public void traverse();
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Algorithms
-basic algorithms
+In this repo we are going to implement algorithms in all languages and all topics.
